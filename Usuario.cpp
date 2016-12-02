611cae6f0b3ed8dec5c1c8409cb36c2a6454b7c9
Jose Fernandez <jrfo_04@unitec.edu>
2016-12-02 13:37:47 -0600

    ok

--- Usuario.cpp
+++ Usuario.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 #include "Usuario.h"
+
+using namespace std;
+
+Usuario::Usuario() {
+
+}//Fin
+
+Usuario::~Usuario() {
+    cout << "Destructor usuario..." << endl;
+}//Fin
+
+void Usuario::setNombre(string nombre) {
+    this->nombre = nombre;
+}//Fin del metodo
+
+void Usuario::setCorreo(string correo) {
+    this->correo = correo;
+}//Fin del metodo
+
+void Usuario::setPassword(string password) {
+    this->password = password;
+}//Fin del metodo
--- Usuario.h
+++ Usuario.h
@@ -1,2 +1,21 @@
 #pragma once
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Usuario {
+    protected:
+        Usuario();
+        void setNombre(string);
+        void setCorreo(string);
+        void setPassword(string);
+        string getNombre();
+        string getCorreo();
+        string getPassword();
+        string nombre;
+        string correo;
+        string password;
+        virtual ~Usuario();
+        virtual int TamContra(string);
+};
